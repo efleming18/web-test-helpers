290301c50ca63462ebd4adb15d3c1ecc3c450600
Eric Fleming <eric-fleming18@hotmail.com>
2016-01-26 21:36:03 -0500

    Added start to first validation rule

---
+++ src/CustomWebTestHelpers/ValidationRules/ValidateJsonResponseValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.WebTesting;
+
+namespace CustomWebTestHelpers.ValidationRules
+{
+    public class ValidateJsonResponseValues : ValidationRule
+    {
+        public override void Validate(object sender, ValidationEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
